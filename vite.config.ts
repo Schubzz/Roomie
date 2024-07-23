4806c627aee9544ece2352444863c3f5432600e8
Alex Schulz <schubz94@gmail.com>
2024-07-23 20:43:06 +0200

    added workbox config in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,44 @@ import react from '@vitejs/plugin-react'
 import {defineConfig} from 'vite'
 import {VitePWA} from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        VitePWA({registerType: 'autoUpdate'}),
         react(),
-        legacy()
+        legacy(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                runtimeCaching: [
+                    {
+                        urlPattern: /^https:\/\/firestore\.googleapis\.com\//,
+                        handler: 'NetworkFirst',
+                        options: {
+                            cacheName: 'firebase-cache',
+                            expiration: {
+                                maxEntries: 50,
+                                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Tage
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200],
+                            },
+                        },
+                    },
+                    {
+                        urlPattern: ({request}) => request.destination === 'document' || request.destination === 'script' || request.destination === 'style',
+                        handler: 'NetworkFirst',
+                        options: {
+                            cacheName: 'assets-cache',
+                            expiration: {
+                                maxEntries: 50,
+                                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Tage
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200],
+                            },
+                        },
+                    }
+                ]
+            }
+        }),
     ],
 })
