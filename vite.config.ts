7bcc1485e10d086f55c4f8de874b6da852dafd74
Alex Schulz <schubz94@gmail.com>
2024-08-05 14:38:19 +0200

    disabled workbox

--- vite.config.ts
+++ vite.config.ts
@@ -7,66 +7,66 @@ export default defineConfig({
         react(),
         VitePWA({
             registerType: 'autoUpdate',
-            workbox: {
-                runtimeCaching: [
-                    {
-                        urlPattern: /^https:\/\/firestore\.googleapis\.com\/google\.firestore\.v1\.Firestore\/Write\/channel.*/,
-                        handler: 'CacheFirst',
-                        options: {
-                            cacheName: 'firebase-write-cache',
-                            expiration: {
-                                maxEntries: 50,
-                                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Tage
-                            },
-                            cacheableResponse: {
-                                statuses: [0, 200],
-                            },
-                        },
-                    },
-                    {
-                        urlPattern: /^https:\/\/firestore\.googleapis\.com\/google\.firestore\.v1\.Firestore\/Listen\/channel.*/,
-                        handler: 'CacheFirst',
-                        options: {
-                            cacheName: 'firebase-listen-cache',
-                            expiration: {
-                                maxEntries: 50,
-                                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Tage
-                            },
-                            cacheableResponse: {
-                                statuses: [0, 200],
-                            },
-                        },
-                    },
-                    {
-                        urlPattern: /^https:\/\/apis\.google\.com\/.*/,
-                        handler: 'NetworkFirst',
-                        options: {
-                            cacheName: 'google-cache',
-                            expiration: {
-                                maxEntries: 50,
-                                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Tage
-                            },
-                            cacheableResponse: {
-                                statuses: [0, 200],
-                            },
-                        },
-                    },
-                    {
-                        urlPattern: ({ request }) => request.destination === 'document' || request.destination === 'script' || request.destination === 'style',
-                        handler: 'NetworkFirst',
-                        options: {
-                            cacheName: 'assets-cache',
-                            expiration: {
-                                maxEntries: 50,
-                                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Tage,
-                            },
-                            cacheableResponse: {
-                                statuses: [0, 200],
-                            },
-                        },
-                    },
-                ],
-            },
+            // workbox: {
+            //     runtimeCaching: [
+            //         {
+            //             urlPattern: /^https:\/\/firestore\.googleapis\.com\/google\.firestore\.v1\.Firestore\/Write\/channel.*/,
+            //             handler: 'CacheFirst',
+            //             options: {
+            //                 cacheName: 'firebase-write-cache',
+            //                 expiration: {
+            //                     maxEntries: 50,
+            //                     maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Tage
+            //                 },
+            //                 cacheableResponse: {
+            //                     statuses: [0, 200],
+            //                 },
+            //             },
+            //         },
+            //         {
+            //             urlPattern: /^https:\/\/firestore\.googleapis\.com\/google\.firestore\.v1\.Firestore\/Listen\/channel.*/,
+            //             handler: 'CacheFirst',
+            //             options: {
+            //                 cacheName: 'firebase-listen-cache',
+            //                 expiration: {
+            //                     maxEntries: 50,
+            //                     maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Tage
+            //                 },
+            //                 cacheableResponse: {
+            //                     statuses: [0, 200],
+            //                 },
+            //             },
+            //         },
+            //         {
+            //             urlPattern: /^https:\/\/apis\.google\.com\/.*/,
+            //             handler: 'NetworkFirst',
+            //             options: {
+            //                 cacheName: 'google-cache',
+            //                 expiration: {
+            //                     maxEntries: 50,
+            //                     maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Tage
+            //                 },
+            //                 cacheableResponse: {
+            //                     statuses: [0, 200],
+            //                 },
+            //             },
+            //         },
+            //         {
+            //             urlPattern: ({ request }) => request.destination === 'document' || request.destination === 'script' || request.destination === 'style',
+            //             handler: 'NetworkFirst',
+            //             options: {
+            //                 cacheName: 'assets-cache',
+            //                 expiration: {
+            //                     maxEntries: 50,
+            //                     maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Tage,
+            //                 },
+            //                 cacheableResponse: {
+            //                     statuses: [0, 200],
+            //                 },
+            //             },
+            //         },
+            //     ],
+            // },
         }),
     ],
 });
